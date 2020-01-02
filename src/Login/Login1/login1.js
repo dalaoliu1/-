a1607521c55b52e76c36f7d34f2e908bad207360
dalaoliu1 <786633235@qq.com>
2020-01-02 13:35:25 +0800

    修改

--- src/Login/Login1/login1.js
+++ src/Login/Login1/login1.js
@@ -1,48 +1,42 @@
 import React from 'react';
-import { Button,Icon,message} from 'antd';
+import { Button,Icon } from 'antd';
 import{Link} from 'react-router-dom';
-import{axios} from 'axios';
 var Login1Css =require('./login1.css')
+
 export default class Login1 extends React.Component{
-    state={
+    constructor(props){
+        super(props)
+       this.state={
         username:'',
-        password:'',
+        password:''
+       }
     }
-    login=()=>{
-        this.props.login(this.state)
-    }
-    handleChange=(name,val)=>{
+    changeValue=e=>{
         this.setState({
-            [name]:val
+            [e.target.name]:e.target.value
         })
     }
-    upload=()=>{
-        var data={
-            "username":this.state.username,
-            "password":this.state.password,
-        }
-        axios.post({
-            url:"/user/personal",
-            data:JSON.stringify(data)
-          }).then(Response=>Response.JSON())
-          .then(result=>{
-            if(result.state===1){
-                alert("登录成功")
-                message.info("用户登录成功")
-            }else if(result.state===2){
-                alert("登录失败，密码错误")
-                message.info("密码错误")
-            }else if(result.state===3){
-                alert("账号错误")
-              message.info("账号错误")
-            }
-          }).catch(e=>{
-              message.error(e);
-          })
-        }
-    
+    login=e=>{
+        //去访问服务器，登录成功
+        if(this.state.username==='123'&&this.state.password==='123'){
+         alert('登录成功');
+         //存放
+         window.localStorage.setItem("username","123");
+         window.localStorage.setItem("token","11111");
+         //跳转页面
+         this.props.history.push('/homepage')
+         
 
+        }else{
+            alert('登录失败');
+        }
+       //取出
+        console.log(window.localStorage.getItem("username"))
+      
+
+    }
     render(){
+       
         return(
             //登录页面
             <div className={Login1Css.login}>
@@ -51,11 +45,12 @@ export default class Login1 extends React.Component{
                 <h2>建模变得更加简单</h2>
                 </header>
                 
-                <div><Icon type="user"/><input  type="text"  placeholder="请输入用户名" className={Login1Css.one} onChange={val=>{this.changeValue('username',val)}}></input></div>
-                <div><Icon type="lock"/><input type="text" placeholder="请输入密码" className={Login1Css.one} onChange={val=>{this.changeValue('password',val)}}></input></div>
+                <div><Icon type="user"/><input  type="text"  placeholder="请输入用户名" name="username" value={this.state.username}  className={Login1Css.one} onChange={this.changeValue}></input></div>
+                <div><Icon type="lock"/><input type="text" placeholder="请输入密码" name="password"  value={this.state.password}  className={Login1Css.one} onChange={this.changeValue}></input></div>
                 <Button className={Login1Css.two} size="large" onClick={this.login}>登录</Button>
                 <Link to="/Register"><Button className={Login1Css.two} size="large" >注册</Button></Link>
-                <div>   
+                <div>
+                    
                   <a href className={Login1Css.number}>社交账号登录 </a> 
                   <a href className={Login1Css.number}>忘记密码?</a>
                   
